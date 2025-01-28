068bbfc3b98ebf3658486a8a3b347ffd8583b56a
Ekaterina Schedlberger <ek.schedl@gmail.com>
2025-01-28 01:07:58 +0100

    first commit

--- obj/Debug/net8.0/PdfApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0/PdfApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PdfApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25b7583ef92fa258120c14f8859069fefa6345a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PdfApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PdfApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
